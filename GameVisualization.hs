9583554dff3c6c120709c8bdf2050859ca04a529
guilhermelameira <gui.l.a@hotmail.com>
2018-10-05 12:10:37 -0700

    Added GameVisualization to convert a board state into a UI board

---
+++ GameVisualization.hs
@@ -0,0 +1,83 @@
+module GameVisualization where
+
+type CellState = Int
+type Board = [[CellState]]
+type UltimateBoard = [[Board]]
+
+type Drawing = [String]
+type DrawingLine = String
+
+get_ultimate_board_drawing :: UltimateBoard -> Int -> Drawing 
+get_ultimate_board_drawing [r1,r2,r3] ai = (get_ub_row_drawing r1 "ABC" ai)++
+                           (get_ub_row_drawing r2 "DEF" (ai-3))++
+                           (get_ub_row_drawing r3 "GHI" (ai-6))
+
+get_ub_row_drawing :: [Board] -> [Char] -> Int -> Drawing
+get_ub_row_drawing [x,y,z] [a,b,c] ai = [(\i -> ((get_board_drawing x (ai==0) a)!!i ++" "++
+                                            (get_board_drawing y (ai==1) b)!!i ++" "++
+                                            (get_board_drawing z (ai==2) c)!!i)) i | i <- [0..9]]
+
+get_board_drawing :: Board -> Bool -> Char -> Drawing
+get_board_drawing [r1,r2,r3] act name = [
+    (border act),                        -- 0  .....................
+    (labels act name),                   -- 1  : [A] 1   2   3     :
+    (separator act),                     -- 2  :   |---|---|---|   :
+    (row act '1' (row_chars r1)),        -- 3  : 1 |   |   |   |   :      
+    (separator act),                     -- 4  :   |---|---|---|   :
+    (row act '2' (row_chars r2)),        -- 5  : 2 |   |   |   |   :
+    (separator act),                     -- 6  :   |---|---|---|   :
+    (row act '3' (row_chars r3)),        -- 7  : 3 |   |   |   |   :
+    (separator act),                     -- 8  :   |---|---|---|   :
+    (border act)]                        -- 9  .....................
+
+row_chars :: [CellState] -> [Char]
+row_chars r = [tc x | x <- r] where
+    tc 0 = ' '
+    tc 1 = 'x'
+    tc 2 = 'o'
+
+border :: Bool -> DrawingLine
+border act = if act then "....................." 
+                      else "                     "
+
+labels :: Bool -> Char -> DrawingLine
+labels act name = add_vertical_border act (" ["++[name]++"] 1   2   3     ") 
+
+separator :: Bool -> DrawingLine
+separator act = add_vertical_border act "   |---|---|---|   " 
+
+row :: Bool -> Char -> [Char] -> DrawingLine
+row act i [a,b,c] = add_vertical_border act (" "++[i]++" | "++[a]++" | "++[b]++" | "++[c]++" |   ")
+
+add_vertical_border :: Bool -> DrawingLine -> DrawingLine
+add_vertical_border act str = if act then ":"++str++":" else " "++str++" "
+
+
+
+draw_ultimate_board :: UltimateBoard -> Int -> IO ()
+draw_ultimate_board ub ai = draw (get_ultimate_board_drawing ub ai)
+
+draw :: Drawing -> IO ()
+draw d = do 
+    putStr "\ESC[2J" -- clears terminal
+    mapM_ putStrLn d
+
+
+
+-- --------------------------------------------------------------------------------------------
+
+emptyUBoard = [ 
+       [ [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]] ],
+       [ [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]] ],
+       [ [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]] ] 
+    ]
+
+testUBoard = [ 
+       [ [[1,0,2],[1,0,2],[0,0,0]], [[0,0,2],[0,0,0],[0,0,0]], [[0,0,1],[0,0,0],[0,1,0]] ],
+       [ [[0,0,0],[0,0,0],[0,0,0]], [[1,0,0],[0,2,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,2]] ],
+       [ [[0,0,1],[0,0,0],[0,0,0]], [[0,0,1],[0,0,2],[0,0,0]], [[0,0,0],[0,2,0],[0,0,0]] ] 
+    ]
+
+board1 = [[0,0,0],[0,0,0],[0,0,0]]
+
+test = draw_ultimate_board testUBoard 4
