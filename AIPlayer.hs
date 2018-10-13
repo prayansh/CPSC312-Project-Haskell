26d55a078d296ffccaecd4660592754719d95eb3
Prayansh Srivastava <prayansh@gmail.com>
2018-10-13 04:41:44 -0700

    add fast play function

--- AIPlayer.hs
+++ AIPlayer.hs
@@ -7,9 +7,39 @@ import TypeDef
 import UltimateTicTacToe
 
 -- Super simple AI player
-simple_player :: Player
-simple_player (State ub actB nextP) = do
+unintelligent_player :: Player
+unintelligent_player (State ub actB nextP) = do
     putStrLn ("Press enter for the AI to place "++(show nextP))
     getLine
     let action = head (get_valid_actions ub actB)
     return action
+
+choose_actions =  [ ChooseBoard 0, ChooseBoard 6, ChooseBoard 5,
+                    ChooseBoard 2, ChooseBoard 3, ChooseBoard 4,
+                    ChooseBoard 8, ChooseBoard 7, ChooseBoard 1]
+placeAt_actions = [ PlaceAt 2 0, PlaceAt 0 2, PlaceAt 0 0,
+                    PlaceAt 2 2, PlaceAt 1 1, PlaceAt 1 0,
+                    PlaceAt 2 1, PlaceAt 1 2, PlaceAt 0 1]
+all_actions = choose_actions ++ placeAt_actions
+simple_player :: Player
+simple_player (State ub actB nextP) = do
+    putStrLn ("Press enter for the AI to place "++(show nextP))
+    getLine
+    let valid_actions = (get_valid_actions ub actB)
+    let action = get_next_best valid_actions all_actions
+    return action
+
+get_next_best :: [Action] -> [Action] -> Action
+get_next_best valid (x:xs)
+    | elem x valid = x
+    | otherwise = get_next_best valid xs
+
+get_next_best _ _ = Invalid
+
+random_player :: Player
+random_player (State ub actB nextP) = do
+    return Invalid
+
+shuffle_player :: Player
+shuffle_player (State ub actB nextP) = do
+    return Invalid
--- GameIO.hs
+++ GameIO.hs
@@ -11,14 +11,17 @@ import UltimateTicTacToe
 import GameHelper
 
 ---- Game Start
-start :: [Player] -> IO()
-start [p1,p2] = play ultimateTicTacToe (ContinueGame emptyState) [p1,p2]
+start :: [Player] -> Bool -> IO()
+start [p1,p2] True = playFast ultimateTicTacToe (ContinueGame emptyState) [p1,p2]
     where emptyState = (State emptyUBoardCell (-1) X)
 
-start _ = do
+start [p1,p2] _ = play ultimateTicTacToe (ContinueGame emptyState) [p1,p2]
+    where emptyState = (State emptyUBoardCell (-1) X)
+
+start _ _ = do
     putStrLn "Must specify two players"
 
-startDefault = start [human_player, simple_player]
+startDefault = start [human_player, unintelligent_player] False
 
 ---- Play functions
 play :: Game -> Result -> [Player] -> IO ()
@@ -34,3 +37,16 @@ play game (ContinueGame (State ub actB playerSymbol)) [currentP, otherP] = do
     putStrLn ((show playerSymbol) ++ ": " ++ (show action))
     let playerList = if isPlaceAt action then [otherP, currentP] else [currentP, otherP]
     play game (game action (State ub actB playerSymbol)) playerList
+
+-- | Reduce output for faster game processing (ideal for AI vs AI)
+playFast :: Game -> Result -> [Player] -> IO ()
+
+playFast _ (EndOfGame winner (State ub actB _)) _ = do
+    draw_ultimate_board ub (-1) (u_board_to_board ub)
+    putStrLn ("Winner " ++ (show winner))
+
+playFast game (ContinueGame (State ub actB playerSymbol)) [currentP, otherP] = do
+    action <- currentP (State ub actB playerSymbol)
+    putStrLn ((show playerSymbol) ++ ": " ++ (show action))
+    let playerList = if isPlaceAt action then [otherP, currentP] else [currentP, otherP]
+    playFast  game (game action (State ub actB playerSymbol)) playerList
