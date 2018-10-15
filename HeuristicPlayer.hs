cb53d3a94c7fc818dd8ebf87de5ef778aa920a38
Prayansh Srivastava <prayansh@gmail.com>
2018-10-14 20:18:43 -0700

    remove unnecessary arguments

--- HeuristicPlayer.hs
+++ HeuristicPlayer.hs
@@ -10,43 +10,43 @@ hmm_player game state = fst (heuristic game state 3)
 heuristic :: Game -> State -> Int -> (Action, Double)
 -- ChooseBoard actions
 heuristic game (State ub (-1) currSym) ttl =
-  argmax (valueChooseAct game (State ub (-1) currSym) currSym ttl) avail
+  argmax (valueChooseAct game (State ub (-1) currSym) ttl) avail
   where
     avail = (get_valid_actions ub (-1))
 -- PlaceAt actions
-heuristic game state ttl = argmax (valuePlaceAct game state currSym ttl) avail
+heuristic game state ttl = argmax (valuePlaceAct game state ttl) avail
   where
-    State ub actB currSym = state
+    State ub actB _ = state
     avail = (get_valid_actions ub actB)
 
-valuePlaceAct :: Game -> State -> Symbol -> Int -> Action -> Double
-valuePlaceAct game st currSym ttl act =
-  heuristicForPlace game (game act st) currSym ttl
+valuePlaceAct :: Game -> State -> Int -> Action -> Double
+valuePlaceAct game st ttl act =
+  heuristicForPlace game (game act st) ttl
 
-valueChooseAct :: Game -> State -> Symbol -> Int -> Action -> Double
-valueChooseAct game st currSym ttl act =
-  heuristicForChoose game (game act st) currSym ttl
+valueChooseAct :: Game -> State -> Int -> Action -> Double
+valueChooseAct game st ttl act =
+  heuristicForChoose game (game act st) ttl
 
-heuristicForPlace :: Game -> Result -> Symbol -> Int -> Double
-heuristicForPlace game (ContinueGame (State ub actB _)) symbol 0 =
+heuristicForPlace :: Game -> Result -> Int -> Double
+heuristicForPlace game (ContinueGame (State ub actB symbol)) 0 =
   heuristicU ub symbol
-heuristicForPlace game (ContinueGame st) symbol ttl =
+heuristicForPlace game (ContinueGame st) ttl =
   (heuristicU ub symbol) - (snd (heuristic game st (ttl - 1)))
   where
-    (State ub _ _) = st
-heuristicForPlace game (EndOfGame winner (State ub actB _)) symbol ttl =
+    (State ub _ symbol) = st
+heuristicForPlace game (EndOfGame winner (State ub actB symbol)) ttl =
   heuristicU ub symbol
 
-heuristicForChoose :: Game -> Result -> Symbol -> Int -> Double
-heuristicForChoose game (ContinueGame st) symbol 0 =
+heuristicForChoose :: Game -> Result -> Int -> Double
+heuristicForChoose game (ContinueGame st) 0 =
   heuristicB ((concat ub) !! actB) symbol
   where
-    (State ub actB _) = st
-heuristicForChoose game (ContinueGame st) symbol ttl =
+    (State ub actB symbol) = st
+heuristicForChoose game (ContinueGame st) ttl =
   heuristicB ((concat ub) !! actB) symbol + (snd (heuristic game st (ttl - 1)))
   where
-    (State ub actB _) = st
-heuristicForChoose game (EndOfGame winner (State ub actB _)) symbol _ = 0 -- TODO wtf
+    (State ub actB symbol) = st
+heuristicForChoose game (EndOfGame winner (State ub actB symbol)) _ = 0 -- TODO wtf
 
 argmax :: Ord v => (e -> v) -> [e] -> (e, v)
 argmax f [e] = (e, f e)
